90fa3b0b7df54deb5011dd2cb4cc4436284ea7c8
Nitya Patel <nityapatel450@gmail.com>
2023-10-25 13:35:39 +0530

    expense tracker project

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+
+import HomePage from "./pages/HomePage";
+// import PremiumActivationButton from './components/Profile/PremiumActivationButton';
+// import DarkThemeToggle from './components/Profile/DarkThemeToggle';
+import ForgetPassword from "./pages/ForgetPassword";
+import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
+import AuthForm from "./components/Auth/AuthForm";
+import { PriventGoBack } from "./PrivateRoutes/PriventGoBack";
+import ProtectedRoutes from "./PrivateRoutes/ProtectedRoutes";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PriventGoBack>
+                <AuthForm />
+              </PriventGoBack>
+            }
+          />
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoutes>
+                <HomePage />
+              </ProtectedRoutes>
+            }
+          />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/forgetPassword" element={<ForgetPassword />} />
+          <Route
+            path="/expenseStore"
+            element={
+              <ProtectedRoutes>
+                <ExpenseForm />
+              </ProtectedRoutes>
+            }
+          />
+        </Routes>
+        {/* <PremiumActivationButton />
+        <DarkThemeToggle /> */}
+      </Layout>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+import "./index.css";
+import App from "./App";
+// import BrowserRouter  from  'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
